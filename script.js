2d4ab4f7f303504f8b479f95203217171de188c2
NoriKoi-Cheed <sabilalrizqip@gmail.com>
2026-04-05 18:03:24 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,3 @@
+function showLove() {
+    document.getElementById("hiddenText").innerHTML="Maaffinn akuu yaa sayangggg, huhuhu sedihhh bangettt. maaafff yyaaa😢❤❤😢, huhu akuu ngerasaa bersalahh bangett, maafinn akuu yaa sayangggg"
+}
